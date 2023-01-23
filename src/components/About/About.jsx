42c4a82032a17f58bee4e0e5cdf50f2850173306
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2023-01-23 12:15:12 -0300

    resume update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,14 +3,14 @@ import { useLang } from '../../lang-context';
 
 import './About.css'
 
-const About = ({box}) => {
+const About = ({ box }) => {
     const { state: { lang } } = useLang();
     const [mounted, setMounted] = useState(false);
     const [show, setShow] = useState(0);
     const slider = useRef(null);
     const [scrolled, setScrolled] = useState(0)
     const scrollAbout = useRef(null)
-    let maxScroll = 0    
+    let maxScroll = 0
 
     const SKILLS = [
         'JavaScript',
@@ -32,12 +32,12 @@ const About = ({box}) => {
 
     const scroller = () => {
         let ele = document.getElementById('scrollAboutContainer');
-        setScrolled(Math.abs(ele.scrollTop * 100) / maxScroll);        
-     }
+        setScrolled(Math.abs(ele.scrollTop * 100) / maxScroll);
+    }
 
     useEffect(() => {
         let scrollContainer
-        if (scrollAbout) {            
+        if (scrollAbout) {
             scrollContainer = document.getElementById('scrollAboutContainer')
             let aux = Math.abs(scrollContainer.scrollHeight - scrollContainer.clientHeight) + 110;
             // eslint-disable-next-line
@@ -45,7 +45,7 @@ const About = ({box}) => {
             scrollContainer.addEventListener('scroll', scroller)
         }
 
-        return () => scrollContainer.removeEventListener('scroll', scroller)        
+        return () => scrollContainer.removeEventListener('scroll', scroller)
         // eslint-disable-next-line
     }, [])
 
@@ -68,7 +68,7 @@ const About = ({box}) => {
         }
     }
 
-     const stopSlideTimer = () => {
+    const stopSlideTimer = () => {
         if (slider.current) {
             clearInterval(slider.current);
             slider.current = null;
@@ -84,43 +84,51 @@ const About = ({box}) => {
         return () => stopSlideTimer();
         // eslint-disable-next-line
     }, [])
-    
+
     useEffect(() => {
         setMounted(false)
     }, [box])
 
-  return (
-    <div className={`projects-container`}>
-        <div className={`box-about`}>
-                
-            <div className='about-text-container'>
-                {mounted && <div className='about-scroll-bar' style={{top: `${scrolled * 2}%`}}></div>}
-                <div className={`about-content ${mounted && 'about-content-visible'}`}  
-                    ref={scrollAbout} id='scrollAboutContainer'>
-                    {lang === 'En' 
-                        ?<p className={`about-text`}>
-                            I am a self-taught who found a new passion in programming, I am forming as a Front end developer (with knowledge on back end too) focused on JavaScript technologies since 2021.<br/>
-                            Currently looking for my first work experience in the IT world.<br/><br/>
-                            I am still studying (you never stop learning) and developing applications mainly with Eeact. 
-                            I love to investigate and learn how to take advantages of new tools.<br/>
-                            I am constantly looking for new ideas, opportunities and experiences to improve myself as a developer.<br/><br/>
-                            I am also a photographer, discipline that strengthened my critical eye when designing web interfaces.
-                        </p>
-                        :<p className={`about-text`}>
-                            Soy autodidacta que encontró una nueva pasión en la programación, desde el 2021 estoy formandome como desarrollador front end (con conocimientos en back end tambíen) orientado a tecnologías Javascript.<br/>
-                            Actualmente estoy buscando mi primer experiencia laboral en el mundo IT.<br/><br/>
-                            Continúo estudiando (nunca paras de aprender) y desarrollando aplicaciones principalmente en React.
-                            Me encanta investigar y aprender como aprovechar nuevas herramientas.<br/>
-                            Estoy constantemente buscando nuevas ideas, oportunidades y experiencias para mejorar como programador.<br/><br/>
-                            También soy fotógrafo, diciplina que fortaleció mi mirada crítica a la hora de diseñár interfaces para web. 
-                        </p>}
-                    <div className='skills-container'>
-                        {SKILLS.map(s => (
-                            <div className='skill-card'>{s}</div>
-                        ))}
+    return (
+        <div className={`projects-container`}>
+            <div className={`box-about`}>
+
+                <div className='about-text-container'>
+                    {mounted && <div className='about-scroll-bar' style={{ top: `${scrolled * 2}%` }}></div>}
+                    <div className={`about-content ${mounted && 'about-content-visible'}`}
+                        ref={scrollAbout} id='scrollAboutContainer'>
+                        {lang === 'En'
+                            ? <p className={`about-text`}>
+                                I am a self-taught who found a new passion in programming, I am forming as a Front end developer (with knowledge on back end too) focused on JavaScript technologies since 2021.<br />
+                                Currently looking for my first work experience in the IT world.<br /><br />
+                                I am still studying (you never stop learning) and developing applications mainly with Eeact.
+                                I love to investigate and learn how to take advantages of new tools.<br />
+                                I am constantly looking for new ideas, opportunities and experiences to improve myself as a developer.<br /><br />
+                                I am also a photographer, discipline that strengthened my critical eye when designing web interfaces.
+                            </p>
+                            : <p className={`about-text`}>
+                                Soy autodidacta que encontró una nueva pasión en la programación, desde el 2021 estoy formandome como desarrollador front end (con conocimientos en back end tambíen) orientado a tecnologías Javascript.<br />
+                                Actualmente estoy buscando mi primer experiencia laboral en el mundo IT.<br /><br />
+                                Continúo estudiando (nunca paras de aprender) y desarrollando aplicaciones principalmente en React.
+                                Me encanta investigar y aprender como aprovechar nuevas herramientas.<br />
+                                Estoy constantemente buscando nuevas ideas, oportunidades y experiencias para mejorar como programador.<br /><br />
+                                También soy fotógrafo, diciplina que fortaleció mi mirada crítica a la hora de diseñar interfaces para web.
+                            </p>}
+
+                        <div className='skills-container'>
+                            {SKILLS.map(s => (
+                                <div className='skill-card'>{s}</div>
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
+
+                {mounted &&
+                    <div className='about-resume-download'>
+                        {lang === 'En'
+                            ? <a className='link-button' href={'https://drive.google.com/file/d/1HgVnEFz6qLbZ_Zyp288uFw5nA3--bIX8/view'} target='_blank' rel='noreferrer'>{'download resume'}</a>
+                            : <a className='link-button' href={'https://drive.google.com/file/d/1GMGpYKMRNpf4J3Rc9I2mwiH4ziS5V_6s/view'} target='_blank' rel='noreferrer'>{'descargar currículum'}</a>}
+                    </div>}
 
                 {mounted && <div className='about-img-container'>
                     <div className='about-images'>
@@ -134,10 +142,10 @@ const About = ({box}) => {
                         <div className={`logo-7 ${show === 7 && 'about-img-visible'}`}></div>
                         <div className={`logo-8 ${show === 8 && 'about-img-visible'}`}></div>
                     </div>
-                </div>}           
-        </div>
-    </div>
-  )
+                </div>}
+            </div>
+        </div >
+    )
 }
 
 export default About
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -35,7 +35,9 @@ const Contact = ({ box }) => {
                 </>
                 <a href='https://www.linkedin.com/in/fabricio-repetto/' target='_bank'>LinkedIn</a>
                 <a href='https://github.com/FabricioJRepetto' target='_bank'>GitHub</a>
-                <a href='https://res.cloudinary.com/dsyjj0sch/image/upload/v1665500157/portfolio-preview/cv_fabricio_repetto_v2_Mesa_de_trabajo_1_bokrud.png' target='_bank'>CV (esp)</a>
+                {lang === 'En'
+                    ? <a href='https://drive.google.com/file/d/1HgVnEFz6qLbZ_Zyp288uFw5nA3--bIX8/view' target='_bank'>Resume (eng)</a>
+                    : <a href='https://drive.google.com/file/d/1GMGpYKMRNpf4J3Rc9I2mwiH4ziS5V_6s/view' target='_bank'>Currículum (esp)</a>}
             </div>}
 
             <div className={`box-contact-container ${open && 'box-contact-container-open'}`}>
